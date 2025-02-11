7986045e3ff210c6beeaa5019bf0f67b56dc7815
Dat Nguyen <=>
2025-02-11 20:50:54 +0700

    fix: Fix show home store fragment

--- app/src/main/java/com/example/food_ordering_mobile_app/ui/store/home/HomeFragment.java
+++ app/src/main/java/com/example/food_ordering_mobile_app/ui/store/home/HomeFragment.java
@@ -21,7 +21,7 @@ public class HomeFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_store_home, container, false);
 
         // Initialize buttons
         goToNotificationBtn = view.findViewById(R.id.goToNotificationBtn);
